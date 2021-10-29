83ce44068f6fda08ca6e99e25bb53933e706431e
ShaderLight <37264318+ShaderLight@users.noreply.github.com>
2021-10-29 10:37:16 +0200

    Updated with complement functions

--- modules/conv.py
+++ modules/conv.py
@@ -1,6 +1,8 @@
 from math import modf
 
 
+# From base-10 to any base (up to 16)
+
 def from_dec_conv(x, to_base, bits=5):
     x_frac, x_int = modf(x)
 
@@ -11,6 +13,8 @@ def from_dec_conv(x, to_base, bits=5):
     
     return from_dec_conv_int(x_int, to_base) + '.' + from_dec_conv_frac(x_frac, to_base, bits)
 
+
+# From any base (up to 16) to base-10
 
 def to_dec_conv(x, from_base):
     negative = 1
@@ -33,6 +37,61 @@ def to_dec_conv(x, from_base):
         return negative * (to_dec_conv_int(x_int, from_base))
 
 
+# From base-2 to u2 (two's complement)
+
+def bin_to_u2(x):
+    if x[0] == '-':
+        if x[1] == '1':
+            x = inversion('0' + x[1:])
+        else:
+            x = inversion(x[1:])
+
+        return bin_add_one_lsb(x)
+    
+    if x[0] == '0':
+        return x
+    return '0' + x
+
+
+# From base-2 to u1 (one's complement)
+
+def bin_to_u1(x):
+    if x[0] == '-':
+        if x[1] == '1':
+            return inversion('0' + x[1:])
+        return inversion(x[1:])
+    
+    if x[0] == '0':
+        return x
+    
+    return '0' + x
+
+
+# From u2 (two's complement) function
+
+def u2_to_dec(x):
+    x_split = x.split('.')
+
+    try:
+        return -1 * 2 ** (len(x_split[0]) - 1) * int(x_split[0][0]) + to_dec_conv(x_split[0][1:], 2) + to_dec_conv(x_split[1], 2)
+    except IndexError:
+        return -1 * 2 ** (len(x_split[0]) - 1) * int(x_split[0][0]) + to_dec_conv(x_split[0][1:], 2)
+
+
+# From u1 (one's comeplement) function
+
+def u1_to_dec(x):
+    x_split = x.split('.')
+
+    try:
+        return -1 * (2 ** (len(x_split[0]) - 1) - 1) * int(x_split[0][0]) + to_dec_conv(x_split[0][1:], 2) + to_dec_conv(x_split[1], 2)
+    except IndexError:
+        return -1 * (2 ** (len(x_split[0]) - 1) - 1) * int(x_split[0][0]) + to_dec_conv(x_split[0][1:], 2)
+
+
+# --- Other functions ---
+# Integral part converting
+
 def to_dec_conv_int(x, from_base):
     output = 0
 
@@ -40,16 +99,6 @@ def to_dec_conv_int(x, from_base):
     for i in range(len(x)):
         output += int(x[i]) * from_base ** (len(x)-i-1)
 
-    return output
-
-
-def to_dec_conv_frac(x, from_base):
-    output = 0
-
-    x = detranslate_values(x)
-    for i in range(len(x)):
-        output += int(x[i]) * from_base ** (-1 - i)
-    
     return output
 
 
@@ -81,6 +130,18 @@ def from_dec_conv_int(x, to_base):
     return output_str
 
 
+# Fractional part converting
+
+def to_dec_conv_frac(x, from_base):
+    output = 0
+
+    x = detranslate_values(x)
+    for i in range(len(x)):
+        output += int(x[i]) * from_base ** (-1 - i)
+    
+    return output
+
+
 def from_dec_conv_frac(x, to_base, bits=5):
     assert x < 1
     
@@ -104,6 +165,8 @@ def from_dec_conv_frac(x, to_base, bits=5):
 
     return output_str
 
+
+# Translation for bases higher than 10
 
 def translate_values(x):
     LETTERS = ('A', 'B', 'C', 'D', 'E', 'F')
@@ -133,3 +196,36 @@ def detranslate_values(x):
                 output.append(c)
     
     return output
+
+
+# Binary operations
+
+def inversion(x):
+    output = ''
+    for i in range(len(x)):
+        if x[i] == '0':
+            output += '1'
+        elif x[i] == '1':
+            output += '0'
+        else:
+            output += x[i]
+
+    return output
+
+
+def bin_add_one_lsb(x):
+    sep_index = ['.', x.find('.')]
+    
+    if sep_index[1] == -1:
+        sep_index = [',', x.find(',')]
+
+    for i in range(len(x)):
+        if x[len(x) - i - 1] == '0':
+            output = x[:len(x) - i - 1] + '1' + '0' * len(x[len(x) - i:])
+
+            if sep_index[1] >= 0:
+                output = output[:sep_index[1]] + sep_index[0] + output[sep_index[1] + 1:]
+
+            return output
+
+    raise OverflowError
